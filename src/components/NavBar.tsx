04efbeb85794b114ba232c7dc53c573162e941ba
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-08 11:17:04 -0400

    About Drawer Added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,15 @@
-'use client'
+"use client";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ModeToggle } from "@/providers/mode-toggle";
@@ -22,9 +33,30 @@ export default function NavBar() {
         </AspectRatio>
       </div>
       <div className="flex items-center gap-5  dark:text-white p-1">
-      <a href="/dashboard" className="rounded-lg hover:bg-slate-100/50 p-2 ">LOGIN</a>
-      <a href="/"  className="rounded-lg hover:bg-slate-100/50 p-2">SOBRE</a>
-      <ModeToggle></ModeToggle>
+        <a href="/dashboard" className="rounded-lg hover:bg-slate-100/50 p-2 ">
+          LOGIN
+        </a>
+        <Drawer>
+          <DrawerTrigger className="text-center rounded-lg hover:bg-slate-100/50 p-2">
+            SOBRE
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle className="text-center">Seja Bem Vindo!</DrawerTitle>
+              <DrawerDescription className="text-center lg:mx-80 mt-2">
+              O Diário Alerta é um aplicativo que oferece a tranquilidade de monitorar se o seu CPF ou CNPJ foi mencionado no Diário Oficial da União. Com uma interface intuitiva, você pode cadastrar seus documentos e receber notificações instantâneas sempre que houver uma citação relevante para você. Esteja sempre informado sobre quaisquer menções legais que envolvam seus dados pessoais ou empresariais, permitindo que você tome medidas proativas, se necessário. Mantenha-se atualizado e protegido com o Diário Alerta!
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter className="text-center">
+              Antonio Carlos de Abreu Junior & Arthur Oliveira da Silva
+              <DrawerClose>
+                <Button variant="outline">Fechar</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+
+        <ModeToggle></ModeToggle>
       </div>
     </div>
   );
