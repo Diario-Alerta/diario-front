f31bc8a0893acf4fa3820447b3b09b5478213439
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-14 08:26:04 -0400

    login screen ui completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/NavBar";
 import Form from "./form";
+import Email from "next-auth/providers/email";
 
 export default function LoginPage() {
   return (
@@ -7,7 +8,12 @@ export default function LoginPage() {
       <div className="border dark:border-gray-500 mt-1 rounded-sm xl:mx-96">
         <NavBar></NavBar>
       </div>
-      <div className="flex flex-col justify-center xl:mx-96 border dark:border-gray-500 rounded-sm mt-1 md:h-[650px] items-center ">
+      <div className="flex flex-col justify-center xl:mx-96 border dark:border-gray-500 rounded-sm mt-1 md:h-[450px] items-center ">
+        <h1 className="mx-10 md:mx-44 text-center text-2xl underline italic">Bem-vindo ao Diario Alerta!</h1>
+        <h1 className="mx-10 md:mx-44 lg:mx-80 mt-2 text-center">
+          Faça login para acessar sua conta e aproveitar todos os recursos e
+          benefícios.
+        </h1>
         <div className="bg-black/70 flex flex-col items-center p-2 rounded-lg mt-2">
           <Form></Form>
           <a
@@ -17,6 +23,11 @@ export default function LoginPage() {
             Voltar
           </a>
         </div>
+      </div>
+      <div className="border dark:border-gray-500 mt-1 rounded-sm xl:mx-96 p-1 gap-1">
+        <h1 className="text-2xl">REGISTRO</h1>
+      <h1>Os registros no sistema sao feitos de forma manual, e podemos cobrar uma taxa pequena dependendo to tamanho da sua consulta. Se você deseja acessar todas as funcionalidades, entre em contato por email.</h1>
+      <h1 className="mt-2 mb-2 text-center text-lg font-semibold">Antoniocarlosjr007@hotmail.com</h1>
       </div>
     </div>
   );
