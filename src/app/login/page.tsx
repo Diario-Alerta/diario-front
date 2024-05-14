3523ea8a23888becc8a16627c072c2c60a927695
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-14 08:34:07 -0400

    Login ui text color fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,8 +9,8 @@ export default function LoginPage() {
         <NavBar></NavBar>
       </div>
       <div className="flex flex-col justify-center xl:mx-96 border dark:border-gray-500 rounded-sm mt-1 md:h-[450px] items-center ">
-        <h1 className="mx-10 md:mx-44 text-center text-2xl underline italic">Bem-vindo ao Diario Alerta!</h1>
-        <h1 className="mx-10 md:mx-44 lg:mx-80 mt-2 text-center">
+        <h1 className="mx-10 md:mx-44 text-center text-2xl underline italic dark:text-white text-[#242D4C]">Bem-vindo ao Diario Alerta!</h1>
+        <h1 className="mx-10 md:mx-44 lg:mx-80 mt-2 text-center dark:text-white text-[#242D4C]">
           Faça login para acessar sua conta e aproveitar todos os recursos e
           benefícios.
         </h1>
@@ -25,9 +25,9 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="border dark:border-gray-500 mt-1 rounded-sm xl:mx-96 p-1 gap-1">
-        <h1 className="text-2xl">REGISTRO</h1>
+        <h1 className="text-2xl dark:text-white text-[#242D4C]">REGISTRO</h1>
       <h1>Os registros no sistema sao feitos de forma manual, e podemos cobrar uma taxa pequena dependendo to tamanho da sua consulta. Se você deseja acessar todas as funcionalidades, entre em contato por email.</h1>
-      <h1 className="mt-2 mb-2 text-center text-lg font-semibold">Antoniocarlosjr007@hotmail.com</h1>
+      <h1 className="mt-2 mb-2 text-center text-lg font-semibold dark:text-white text-[#242D4C]">Antoniocarlosjr007@hotmail.com</h1>
       </div>
     </div>
   );
